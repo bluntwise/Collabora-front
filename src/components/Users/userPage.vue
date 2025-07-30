6aa3e1502d809ca199e9c0b34b48d79f532ea2c8
bluntwise <alandely11@gmail.com>
2025-07-31 00:21:53 +0200

    [ADD] onDelete fix to manage differents errors

--- src/components/Users/userPage.vue
+++ src/components/Users/userPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="response">
     <ul v-if="response">
-      <li v-for="user in response" :key="user._id" class="users">
+      <li v-for="user in response" :key="user.id" class="users">
         <strong>{{ user.firstName }} {{ user.lastName }}</strong> -
         <em>{{ user.role }}</em> ({{ user.email }})
         <button class="btn-delete" @click="onDelete(user.id)">
@@ -24,6 +24,7 @@ import useAPIRequest from "@/api/useAPIRequest.js";
 
 const response = ref([]); // Déclarer response comme réactive
 const { request: deleteRequest, errorMessage: errDelete, loading } = useAPIRequest({ method: 'DELETE' });
+const { request: getRequest } = useAPIRequest({ method: 'GET' });
 
 onMounted(async () => {
 
@@ -32,14 +33,36 @@ onMounted(async () => {
    response.value = await request({endpoint : "/users"})
 });
 
+
 async function onDelete(userId) {
-  try{
-    const response = await deleteRequest({ endpoint: `/users/${userId}` });
-    console.log(response);
-  }catch (error) {
-    console.log(error);
+  try {
+    await deleteRequest({ endpoint: `/users/${userId}` });
+    // Mise à jour correcte de la liste
+    let userDelete = response.value.filter(user => user.id === userId)[0];
+
+    console.log(userDelete);
+    response.value = response.value.filter(user => user.id !== userId);
+    window.toast(response?.message || `Utilisateur ${userDelete.firstName} supprimé avec succès.`, 'success');
+
+  } catch (error) {
+
+    // Message par défaut
+    let msg = 'Une erreur est survenue.';
+
+    // Si backend a bien répondu avec un message
+    if (error?.response?.data?.message) {
+      msg = error.response.data.message;
+    }
+
+    // Si backend ne répond pas ou autre souci
+    else if (error?.message) {
+      msg = error.message;
+    }
+
+    window.toast(msg, 'error');
   }
 }
+
 </script>
 
 <style scoped>
--- src/components/ui/BaseToast.vue
+++ src/components/ui/BaseToast.vue
@@ -1,62 +1,76 @@
-<template>
-  <div
-      v-if="show"
-      class="toast"
-      :class="type"
-      :style="{ transform: isHiding ? 'translateX(200%)' : 'translateX(0)' }"
-  >
-    {{ message }}
-  </div>
-</template>
+  <template>
+    <div
+        v-if="show"
+        class="toast"
+        :class="type"
+        :style="{ transform: isHiding ? 'translateX(200%)' : 'translateX(0)' }"
+    >
+      {{ message }}
+    </div>
+  </template>
 
 
-<script setup>
-  import { ref, onMounted } from 'vue'
+  <script setup>
+    import { ref, onMounted } from 'vue'
 
-  const show = ref(false);
-  const message = ref('');
-  const type = ref('success');
-  const isHiding = ref(false);
+    const show = ref(false);
+    const message = ref('');
+    const type = ref('success');
+    const isHiding = ref(false);
 
-  let timeoutId = null;
+    let timeoutId = null;
 
-  const showToast = (msg, toastType = 'success', duration = 3000) => {
+    const showToast = (msg, toastType = 'success', duration = 3000) => {
 
-    message.value = msg;
-    type.value = toastType;
-    show.value = true;
-    isHiding.value = false;
+      message.value = msg;
+      type.value = toastType; // sucess or error
+      show.value = true;
+      isHiding.value = false;
 
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-      isHiding.value = true;
-      setTimeout(() => {
-        show.value = false
-      }, 500)
-    },duration);
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        isHiding.value = true;
+        setTimeout(() => {
+          show.value = false
+        }, 500)
+      },duration);
+    }
+
+    const toastStyle = {
+
+    }
+    onMounted(() => {
+      window.toast = showToast
+    })
+  </script>
+
+
+  <style scoped>
+  .toast.success{
+    position: fixed;
+    top: 6rem;
+    right: 1rem;
+    padding: 1em 2em;
+    background: white;
+    color: black;
+    border: 3px solid yellowgreen;
+    border-radius: 0.5em;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    z-index: 0;
+    transition: transform 0.5s ease; /* Transition fluide */
   }
 
-  const toastStyle = {
-
+  .toast.error {
+    position: fixed;
+    top: 6rem;
+    right: 1rem;
+    padding: 1em 2em;
+    border-radius: 0.5em;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    z-index: 0;
+    transition: transform 0.5s ease; /* Transition fluide */
+    border-color: #e74c3c;
+    background: #ffe6e6;
   }
-  onMounted(() => {
-    window.toast = showToast
-  })
-</script>
 
-
-<style scoped>
-.toast {
-  position: fixed;
-  top: 6rem;
-  right: 1rem;
-  padding: 1em 2em;
-  background: white;
-  color: black;
-  border: 3px solid yellowgreen;
-  border-radius: 0.5em;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  z-index: 0;
-  transition: transform 0.5s ease; /* Transition fluide */
-}
-</style>
+  </style>
